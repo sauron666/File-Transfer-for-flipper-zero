bda13a1737607e64aff720abc446d7f9f23b367b
sauron666 <thepokergambler@gmail.com>
2024-11-04 22:17:33 +0200

    Update file_transfer.c

--- file_transfer.c
+++ file_transfer.c
@@ -3,8 +3,6 @@
 #include <input/input.h>
 #include <storage/storage.h>
 #include <notification/notification_messages.h>
-#include <usb/class/hid/usb_hid.h>
-#include <string.h>
 
 typedef enum {
     Mode_None,
@@ -12,62 +10,50 @@ typedef enum {
     Mode_Receive
 } TransferMode;
 
-// Глобална променлива за текущия режим на трансфер
 TransferMode current_mode = Mode_None;
 
-// Функция за инициализация на HID интерфейса
-void setup_hid() {
-    usb_init();
-    usb_hid_init();
-    usb_start();
+// Функция за записване на данни във файл
+void receive_file() {
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+    File* file = storage_file_open(storage, "/ext/received_file.txt", FSAM_WRITE, FSOM_CREATE_ALWAYS);
+
+    if (!file) {
+        furi_record_close(RECORD_STORAGE);
+        return;
+    }
+
+    const char* data = "Example data to store in file";
+    storage_file_write(file, data, strlen(data));
+    storage_file_close(file);
+    furi_record_close(RECORD_STORAGE);
 }
 
-// Функция за изпращане на файл към компютъра през HID
-void send_file(const char* file_path) {
+// Функция за четене на данни от файл
+void send_file() {
     Storage* storage = furi_record_open(RECORD_STORAGE);
-    File* file = storage_file_open(storage, file_path, FSAM_READ, FSOM_OPEN_EXISTING);
+    File* file = storage_file_open(storage, "/ext/received_file.txt", FSAM_READ, FSOM_OPEN_EXISTING);
 
-    if(file) {
-        uint8_t buffer[1];
-        while(storage_file_read(file, buffer, sizeof(buffer)) > 0) {
-            usb_hid_send(buffer, sizeof(buffer));
-        }
-        // Изпраща специален байт за край на трансфера
-        buffer[0] = 0xFF;
-        usb_hid_send(buffer, sizeof(buffer));
+    if (file) {
+        char buffer[64];
+        storage_file_read(file, buffer, sizeof(buffer) - 1);
+        buffer[sizeof(buffer) - 1] = '\0';  // Добавяне на край на стринга
+        // Може да се използва buffer за показване на данните на дисплея или други операции
         storage_file_close(file);
     }
     furi_record_close(RECORD_STORAGE);
 }
 
-// Функция за получаване на файл от компютъра през HID
-void receive_file(const char* save_path) {
-    Storage* storage = furi_record_open(RECORD_STORAGE);
-    File* file = storage_file_open(storage, save_path, FSAM_WRITE, FSOM_CREATE_ALWAYS);
-
-    if(file) {
-        uint8_t buffer[1];
-        while(true) {
-            usb_hid_recv(buffer, sizeof(buffer));
-            if(buffer[0] == 0xFF) break;  // Край на трансфера
-            storage_file_write(file, buffer, sizeof(buffer));
-        }
-        storage_file_close(file);
-    }
-    furi_record_close(RECORD_STORAGE);
-}
-
-// Основна функция за графичния интерфейс на приложението
+// Графичен интерфейс на основното меню
 void draw_main_menu(Canvas* canvas) {
     canvas_clear(canvas);
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 20, 20, "File Transfer");
+    canvas_draw_str(canvas, 10, 20, "File Transfer");
     canvas_draw_str(canvas, 10, 40, "1. Send File");
     canvas_draw_str(canvas, 10, 60, "2. Receive File");
     canvas_draw_str(canvas, 10, 80, "Press 1 or 2 to choose");
 }
 
-// Обработка на входа от бутоните
+// Обработка на входните данни
 void input_callback(InputEvent* input_event, void* ctx) {
     if(input_event->type == InputTypePress) {
         if(input_event->key == InputKey1) {
@@ -78,15 +64,11 @@ void input_callback(InputEvent* input_event, void* ctx) {
     }
 }
 
-// Основна функция за приложението
+// Основна функция на приложението
 int32_t file_transfer_app(void* p) {
     Gui* gui = furi_record_open(RECORD_GUI);
     NotificationApp* notification = furi_record_open(RECORD_NOTIFICATION);
 
-    // Инициализация на HID
-    setup_hid();
-
-    // Настройка на графичния интерфейс и обработка на входа
     ViewPort* viewport = view_port_alloc();
     view_port_draw_callback_set(viewport, draw_main_menu, NULL);
     view_port_input_callback_set(viewport, input_callback, NULL);
@@ -98,12 +80,10 @@ int32_t file_transfer_app(void* p) {
 
     if(current_mode == Mode_Send) {
         notification_message(notification, &sequence_set_only_red);
-        const char* file_path = "/ext/your_file.bin";  // Път до файла на SD картата
-        send_file(file_path);
+        send_file();
     } else if(current_mode == Mode_Receive) {
         notification_message(notification, &sequence_set_only_green);
-        const char* save_path = "/ext/received_file.bin";
-        receive_file(save_path);
+        receive_file();
     }
 
     notification_message(notification, &sequence_reset_red);
